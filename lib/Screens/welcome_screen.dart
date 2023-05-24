6c99a0e48bb6dcff9a1fdaeff4c7eff929a477d9
sachin <baralisachin441@gmail.com>
2023-05-24 08:25:22 +0545

    travel app

---
+++ lib/Screens/welcome_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo section
+          Center(
+            child: Image.asset(
+              'lib/assets/icons/logo.png',
+              height: 200,
+              width: 200,
+            ),
+          ),
+
+          //elevated button section
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('');
+            },
+            child: const Text('Let\'s Go'),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:travel_diary/Screens/welcome_screen.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const SplashScreen(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+      },
+    );
+  }
+}
